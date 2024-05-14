e1610ff82e7a36760ebd5418d16b9a95cd96db8f
rohxnn <rohanreji123@gmail.com>
2024-05-14 14:37:01 +0530

    feat(UploadCOmponent): add upload logic using parse library

---
+++ src/components/UploadComponent/UploadComponent.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import './UploadComponent.css'
+//parser
+import Papa from 'papaparse';
+//component
+import DataComponent from "../DataComponent/DataComponent";
+
+function UploadComponent() {
+
+    const [result, setResults] = useState([]);
+
+    const handleUploadChange = (e) => {
+        Papa.parse(e.target.files[0], {
+            header: true,
+            skipEmptyLines: true,
+            complete: ((results) => {
+                if (results.data) {
+                    console.log(results.data);
+                    setResults(results.data);
+                    console.log(result)
+                }
+            })
+        });
+    }
+
+    return (
+        <>
+            <div className="header-container">
+                <label htmlFor="file" className="upload-button">
+                    Upload
+                </label>
+                <input
+                    id="file"
+                    type="file"
+                    name="file"
+                    accept=".csv"
+                    onChange={(e) => handleUploadChange(e)}
+                    style={{ display: 'none' }}
+                />
+
+            </div>
+            <div>
+                <DataComponent results={result}></DataComponent>
+            </div>
+        </>
+
+    );
+}
+
+export default UploadComponent;
