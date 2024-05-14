7a5627dbbd7f09b780ab033742cd3f413df848f5
rohxnn <rohanreji123@gmail.com>
2024-05-14 15:24:01 +0530

    feat(app):add header

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import Home from './components/home/Home';
 
 function App() {
   return (
+    <>
+    <header>
+    <h1>Dynamic CSV Editor</h1>
+    </header>
     <div className='container'>
       <Home></Home>
     </div>
+    </>
   );
 }
 
